3ecc339866e6bc4c2de619fc294b26bde3fc1751
Symon <59005260+4SAnalyticsnModelling@users.noreply.github.com>
2025-01-03 22:03:09 -0700

    Update blk6.zig

--- src/globalStructs/blk6.zig
+++ src/globalStructs/blk6.zig
@@ -1,21 +1,21 @@
 const std = @import("std");
 
 pub const Blk6 = struct {
-    azi: comptime f32 = 0,
-    rmax: comptime f32 = 0,
-    dec: comptime f32 = 0,
-    tavg1: comptime f32 = 0,
-    tavg2: comptime f32 = 0,
-    tavg3: comptime f32 = 0,
-    amp1: comptime f32 = 0,
-    amp2: comptime f32 = 0,
-    amp3: comptime f32 = 0,
-    vavg1: comptime f32 = 0,
-    vavg2: comptime f32 = 0,
-    vavg3: comptime f32 = 0,
-    vmp1: comptime f32 = 0,
-    vmp2: comptime f32 = 0,
-    vmp3: comptime f32 = 0,
+    azi: comptime f32 = 0.0,
+    rmax: comptime f32 = 0.0,
+    dec: comptime f32 = 0.0,
+    tavg1: comptime f32 = 0.0,
+    tavg2: comptime f32 = 0.0,
+    tavg3: comptime f32 = 0.0,
+    amp1: comptime f32 = 0.0,
+    amp2: comptime f32 = 0.0,
+    amp3: comptime f32 = 0.0,
+    vavg1: comptime f32 = 0.0,
+    vavg2: comptime f32 = 0.0,
+    vavg3: comptime f32 = 0.0,
+    vmp1: comptime f32 = 0.0,
+    vmp2: comptime f32 = 0.0,
+    vmp3: comptime f32 = 0.0,
 
     pub fn init() Blk6 {
         return .{};
